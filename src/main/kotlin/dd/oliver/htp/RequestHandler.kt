e44474a72cc1ec62a0a7cbbcb20e7b07fbbbc9c0
OliverDD <oliverdd@qq.com>
2020-09-15 09:02:42 +0800

    Add html, icon to show directory and file

--- src/main/kotlin/dd/oliver/htp/RequestHandler.kt
+++ src/main/kotlin/dd/oliver/htp/RequestHandler.kt
@@ -37,46 +37,89 @@ class RequestHandler(val basePath: String) : SimpleChannelInboundHandler<HttpReq
     }
 
     override fun channelRead0(ctx: ChannelHandlerContext, msg: HttpRequest) {
-        val path = basePath + msg.uri()
-        logger.debug("Fetching $path")
-        val file = File(path)
-        val rfile = RandomAccessFile(file, "r")
-        if (msg.headers().contains("Range")) { // Multiple threads download
-            logger.debug("Find 'Range' in HttpRequest: ${msg.headers().get("Range")}")
-            val range = msg.headers().get("Range").substring(6) // remove 'bytes='
-            // Line
-            val response = DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.PARTIAL_CONTENT)
-            val bIdx = range.substring(0, range.indexOf("-")).toLong()
-            var eIdx = 0L
-            if (range.indexOf("-") == range.length - 1) { // example: 0-
-                // TODO: Judge bIdx > file.size?
-                eIdx = rfile.length() - 1
-            } else { // example: 0-100
-                eIdx = range.substring(range.indexOf("-") + 1, range.length).toLong()
-            }
-            logger.debug("bIdx = ${bIdx}; eIdx = ${eIdx}")
-            // Headers
-            response.headers().set("Accept-Ranges", "bytes")
-            response.headers().set("Content-Range", "bytes ${bIdx}-${eIdx}/${rfile.length()}")
-            response.headers().set("Content-Disposition", "attachment; filename=\"${file.name}\"")
-            response.headers().set("Content-Type", "application/octet-stream")
-            response.headers().set("Content-Length", "${eIdx - bIdx + 1}")
-            // Content
-            response.content().writeBytes(rfile.channel, bIdx, (eIdx - bIdx + 1).toInt())
-            ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE)
-        } else { // Single thread download
-            logger.debug("No 'Range' in HttpRequest")
+        if (msg.uri() == "/favicon.ico") {
             // Line
             val response = DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK)
             // Headers
-            response.headers().set("Accept-Ranges", "bytes")
-            response.headers().set("Content-Range", "bytes ${0}-${rfile.length() - 1}/${rfile.length()}")
-            response.headers().set("Content-Disposition", "attachment; filename=\"${file.name}\"")
-            response.headers().set("Content-Type", "application/octet-stream")
-            response.headers().set("Content-Length", "${rfile.length()}")
+            response.headers().set("Content-Type", "image/vnd.microsoft.icon")
             // Content
-            response.content().writeBytes(rfile.channel, 0L, rfile.length().toInt())
+            response.content().writeBytes(RequestHandler::class.java.getResourceAsStream("/img/file.png").readBytes())
             ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE)
+        } else {
+            val path = basePath + msg.uri() // msg.uri() example: / or /a/b or /a/b/c.txt
+            logger.debug("Fetching $path")
+            val file = File(path)
+            if (file.isFile) {
+                val rfile = RandomAccessFile(file, "r")
+                if (msg.headers().contains("Range")) { // Multiple threads download
+                    logger.debug("Find 'Range' in HttpRequest: ${msg.headers().get("Range")}")
+                    val range = msg.headers().get("Range").substring(6) // remove 'bytes='
+                    // Line
+                    val response = DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.PARTIAL_CONTENT)
+                    val bIdx = range.substring(0, range.indexOf("-")).toLong()
+                    var eIdx = 0L
+                    if (range.indexOf("-") == range.length - 1) { // example: 0-
+                        // TODO: Judge bIdx > file.size?
+                        eIdx = rfile.length() - 1
+                    } else { // example: 0-100
+                        eIdx = range.substring(range.indexOf("-") + 1, range.length).toLong()
+                    }
+                    logger.debug("bIdx = ${bIdx}; eIdx = ${eIdx}")
+                    // Headers
+                    response.headers().set("Accept-Ranges", "bytes")
+                    response.headers().set("Content-Range", "bytes ${bIdx}-${eIdx}/${rfile.length()}")
+                    response.headers().set("Content-Disposition", "attachment; filename=\"${file.name}\"")
+                    response.headers().set("Content-Type", "application/octet-stream")
+                    response.headers().set("Content-Length", "${eIdx - bIdx + 1}")
+                    // Content
+                    response.content().writeBytes(rfile.channel, bIdx, (eIdx - bIdx + 1).toInt())
+                    ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE)
+                } else { // Single thread download
+                    logger.debug("No 'Range' in HttpRequest")
+                    // Line
+                    val response = DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK)
+                    // Headers
+                    response.headers().set("Accept-Ranges", "bytes")
+                    response.headers().set("Content-Range", "bytes ${0}-${rfile.length() - 1}/${rfile.length()}")
+                    response.headers().set("Content-Disposition", "attachment; filename=\"${file.name}\"")
+                    response.headers().set("Content-Type", "application/octet-stream")
+                    response.headers().set("Content-Length", "${rfile.length()}")
+                    // Content
+                    response.content().writeBytes(rfile.channel, 0L, rfile.length().toInt())
+                    ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE)
+                }
+            } else {
+                val builder = StringBuilder()
+                builder.append(
+                    """
+                <!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <title>Htp Server</title>
+                </head>
+                <body>
+                    <dl>
+                """.trimIndent()
+                )
+                file.list().forEach {
+                    builder.append("<dt><a href=\"${msg.uri() + it + "/"}\">${it}</a></dt>")
+                }
+                builder.append(
+                    """
+                    </dl>
+                </body>
+                </html>
+            """.trimIndent()
+                )
+                // Line
+                val response = DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK)
+                // Headers
+                response.headers().set("Content-Type", "text/html; charset=utf-8")
+                // Content
+                response.content().writeBytes(builder.toString().toByteArray())
+                ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE)
+            }
         }
     }
 
