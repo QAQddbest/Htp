651131ce4ddada5242ac05c06a8907adf38a1c48
OliverDD <oliverdd@qq.com>
2020-09-21 10:57:15 +0800

    Arrange version and update README.md file.

--- README.md
+++ README.md
@@ -16,3 +16,43 @@
 
 ---
 
+## Introduce
+
+Htp is a simple online file system, a console application based on netty, clikt and kotest.
+
+You can run Htp on every path in your file system, which will deploy that directory into web, offering  ability of browsing directory and downloading file.
+
+## Advantage
+
+1. Multiply thread downloading. Support 'Range' in http header.
+2. Never OOM. Use chunked to transfer file, which makes it impossible to run into OOM.
+3. Long connection supported. Support 'Connection' in http header.
+
+## How to use
+
+> **JRE is required!**
+
+Browse into `bin` directory and you can find two executable file there.
+
+`htp` is prepared for \*nix, and `htp.bat` is prepared for windows.
+
+run: `htp -h` to display helping message.
+
+```
+> htp -h
+Usage: htp [OPTIONS] PATH
+
+Options:
+  --version       Show the version and exit
+  -p, --port INT  Port to deploy
+  -h, --help      Show this message and exit
+
+Arguments:
+  PATH  Path to display
+```
+
+As you can see, `PATH` is required and `port` is alternative.
+
+## TODO
+
+Not yet, at last before I master how to upload file. XD
--- src/main/kotlin/dd/oliver/htp/Entry.kt
+++ src/main/kotlin/dd/oliver/htp/Entry.kt
@@ -2,26 +2,33 @@ package dd.oliver.htp
 
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.options.default
+import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.options.versionOption
+import com.github.ajalt.clikt.parameters.types.int
 import com.github.ajalt.clikt.parameters.types.path
 import org.slf4j.LoggerFactory
+import java.net.InetAddress
+
 
 private val logger = LoggerFactory.getLogger("Main")
 
 class Htp : CliktCommand() {
     init {
-        versionOption("1.0.0")
+        versionOption("1.0.1")
     }
 
+    private val port by option("-p", "--port", help = "Port to deploy").int().default(2333)
     private val path by argument(help = "Path to display").path(mustExist = true, canBeFile = false)
 
     override fun run() {
         val file = path.toFile()
         val canPath = file.canonicalPath.replace("\\", "/")
         logger.info("Pick up path $canPath")
-
+        val addr = InetAddress.getLocalHost()
+        logger.info("Now browse http://${addr.hostAddress}:${port}")
         val htpServer = HtpServer(canPath)
-        htpServer.run(2333)
+        htpServer.run(port)
     }
 }
 
